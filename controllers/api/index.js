d0a746ee11dd4f3c29be8a2cf8a1876c5ad1b74b
Oliver Shih <olivershih@gmail.com>
2021-05-05 21:02:44 -0700

    finished setting file structure

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 
-// const userRoutes = require("./userRoutes");
-// const postRoutes = require('./postRoutes');
-// const commentRoutes = require('./commentRoutes');
+const workoutRoutes = require("./workoutRoutes");
 
-// router.use("/users", userRoutes);
-// router.use('/post', postRoutes);
-// router.use('/comment', commentRoutes);
+router.use("/workouts", workoutRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const router = require("./controllers");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require('./models');
+// const db = require('./models');
 
 const app = express();
 
